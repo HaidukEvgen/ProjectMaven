d8584f8060f03d41017fa6a9234ab9c78b43dfa1
HaidukEvgen <kanura56@gmail.com>
2021-11-24 18:13:24 +0300

    all last tests

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -255,20 +255,62 @@ class ConverterTest {
         actual = Converter.convertSpeed(choice, output, input);
         assertEquals(expected, actual);
     }
-    @Test
-    void testConvertTemperature(){
-        int choice = 1;
-        int output = 2;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "1, 3",
+            "2, 1",
+            "2, 3",
+            "3, 1",
+            "3, 2",
+    })
+    void testConvertTemperature(int choice, int output){
         double input = 1;
         boolean expected = true;
         boolean actual;
         actual = Converter.convertTemperature(choice, output, input);
         assertEquals(expected, actual);
     }
-    @Test
-    void testConvertWeight(){
-        int choice = 1;
-        int output = 2;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "1, 3",
+            "1, 4",
+            "1, 5",
+            "1, 6",
+            "2, 1",
+            "2, 3",
+            "2, 4",
+            "2, 5",
+            "2, 6",
+            "3, 1",
+            "3, 2",
+            "3, 4",
+            "3, 5",
+            "3, 6",
+            "4, 1",
+            "4, 2",
+            "4, 3",
+            "4, 5",
+            "4, 6",
+            "5, 1",
+            "5, 2",
+            "5, 3",
+            "5, 4",
+            "5, 6",
+            "6, 1",
+            "6, 2",
+            "6, 3",
+            "6, 4",
+            "6, 5",
+            "7, 1",
+            "7, 2",
+            "7, 3",
+            "7, 4",
+            "7, 5",
+            "7, 6",
+    })
+    void testConvertWeight(int choice, int output){
         char grad = 176;
         double input = 1;
         double result = 0;
