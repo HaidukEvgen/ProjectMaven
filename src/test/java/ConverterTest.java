a1af883595341dc06f88bd494d96364b0976b285
HaidukEvgen <kanura56@gmail.com>
2021-11-24 18:05:56 +0300

    all volume tests

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -191,10 +191,46 @@ class ConverterTest {
         actual = Converter.convertSquare(choice, output, input);
         assertEquals(expected, actual);
     }
-    @Test
-    void testConvertVolume(){
-        int choice = 1;
-        int output = 2;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "1, 3",
+            "1, 4",
+            "1, 5",
+            "1, 6",
+            "2, 1",
+            "2, 3",
+            "2, 4",
+            "2, 5",
+            "2, 6",
+            "3, 1",
+            "3, 2",
+            "3, 4",
+            "3, 5",
+            "3, 6",
+            "4, 1",
+            "4, 2",
+            "4, 3",
+            "4, 5",
+            "4, 6",
+            "5, 1",
+            "5, 2",
+            "5, 3",
+            "5, 4",
+            "5, 6",
+            "6, 1",
+            "6, 2",
+            "6, 3",
+            "6, 4",
+            "6, 5",
+            "7, 1",
+            "7, 2",
+            "7, 3",
+            "7, 4",
+            "7, 5",
+            "7, 6",
+    })
+    void testConvertVolume(int choice, int output){
         double rate = 100;
         double input = 1;
         double result = 0;
