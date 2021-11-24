07bbeb3d2f7b046b618093125169acf978083bb4
HaidukEvgen <kanura56@gmail.com>
2021-11-24 18:03:39 +0300

    all square tests

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -139,18 +139,60 @@ class ConverterTest {
         actual = Converter.convertTime(choice, output, input);
         assertEquals(expected, actual);
     }
-    @Test
-    void testConvertSquare(){
-        int choice = 1;
-        int output = 2;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "1, 3",
+            "1, 4",
+            "1, 5",
+            "1, 6",
+            "1, 7",
+            "2, 1",
+            "2, 3",
+            "2, 4",
+            "2, 5",
+            "2, 6",
+            "2, 7",
+            "3, 1",
+            "3, 2",
+            "3, 4",
+            "3, 5",
+            "3, 6",
+            "3, 7",
+            "4, 1",
+            "4, 2",
+            "4, 3",
+            "4, 5",
+            "4, 6",
+            "4, 7",
+            "5, 1",
+            "5, 2",
+            "5, 3",
+            "5, 4",
+            "5, 6",
+            "5, 7",
+            "6, 1",
+            "6, 2",
+            "6, 3",
+            "6, 4",
+            "6, 5",
+            "6, 7",
+            "7, 1",
+            "7, 2",
+            "7, 3",
+            "7, 4",
+            "7, 5",
+            "7, 6",
+    })
+    void testConvertSquare(int choice, int output){
         double input = 1;
         boolean expected = true;
         boolean actual;
         actual = Converter.convertSquare(choice, output, input);
         assertEquals(expected, actual);
     }
-        @Test
-        void testConvertVolume(){
+    @Test
+    void testConvertVolume(){
         int choice = 1;
         int output = 2;
         double rate = 100;
